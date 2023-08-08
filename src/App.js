6148fda9585b844da05308a7e1849fbf3ff53566
Aure <turduf3@gmail.com>
2023-08-08 14:58:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Expenses from "./components/Expenses";
+
+function App() {
+  const expenses= [
+    {
+      id: "e1",
+      title: "Auto comprata",
+      amount: 12900.0,
+      date: new Date(2022, 12, 14),
+    },
+    {
+      id: "e2",
+      title: "Microonde",
+      amount: 125.0,
+      date: new Date(2022, 10, 25),
+    },
+    {
+      id: "e3",
+      title: "Lavatrice",
+      amount: 400.0,
+      date: new Date(2022, 1, 10),
+    },
+  ];
+ 
+
+  return (
+   <Expenses item={expenses}/>
+  );
+}
+
+export default App;
